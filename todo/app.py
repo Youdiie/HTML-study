912ac9451fdb22bc91f51ff3543329cb50a9266a
Youdiie <faengiman@gmail.com>
2022-09-13 20:05:09 +0900

    템플릿 상속

--- todo/app.py
+++ todo/app.py
@@ -6,8 +6,19 @@ todos = ["Meet Karina", "Sana", "Found OnePiece"]
 
 
 @app.route("/")
-def run():
-    name = request.args.to_dict()["name"]
+def run_base():
+    return render_template("base.html")
+
+
+@app.route("/head")
+def run_head():
+    return render_template("head.html")
+
+
+@app.route("/todos")
+def run_todos():
+    # name = request.args.to_dict()["name"]
+    name = request.args.get("name", "None")
     return render_template("index.html", name=name, todos=todos)
 
 
